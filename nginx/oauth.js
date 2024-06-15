222646196f7cfa350bcf8a31b6b7e47773f04874
ShyamPrgrmr <www.shyam.pradhan@gmail.com>
2024-06-15 13:25:53 +0000

    Final Commit

---
+++ nginx/oauth.js
@@ -0,0 +1,33 @@
+/var/log/nginx # cat /etc/nginx/js/oauth.js
+function introspectAccessToken(r) {
+        var auth_head = r.headersIn['Authorization'];
+        var token = auth_head.split(" ")[1]
+        var auth_head = "token="+token+"&scope=api"
+
+        ngx.fetch('http://172.105.55.15:4445/admin/oauth2/introspect', {
+                        method: 'POST',
+                        headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                        },
+                        body: auth_head
+        }).then(response => {
+                        return response.text();
+        }).then(result=>{return JSON.parse(result);
+        }).then(body => {
+
+                if(body.active){
+                   r.error("info","Token is valid. TOKEN = "+token);
+                   r.return(204,body);
+                }
+                else{
+                  r.error("token is not valid")
+                  r.return(401,body);
+                }
+        }).catch(err => {
+                r.error('Fetch error: ' + err);
+                r.return(500, 'Internal Server Error');
+        });
+}
+
+export default { introspectAccessToken }
+
